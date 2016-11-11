74f4b2ea44a6e0bec8ef33ce3903d35bf1e44faa
philip-long <philip.long.nantes@gmail.com>
2016-11-11 11:40:34 +0100

    finised supermaket exercise

--- 2/supermarche.cc
+++ 2/supermarche.cc
@@ -1,6 +1,110 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
+int total_tills=0;
+
+class Article{
+	private:
+		const string nom_; // const string as string is object
+		double prix_;
+		bool on_sale_;
+	public:
+		Article (string const& nom,double prix, bool on_sale=false):
+				nom_(nom),prix_(prix),on_sale_(on_sale){}
+		Article (const Article& autre) :
+				nom_(autre.nom_),prix_(autre.prix_),on_sale_(autre.on_sale_){}
+		
+		string name() const{
+			return nom_;
+		} 
+		double price() const{
+			return prix_;
+		}
+		bool on_sale() const {
+			return on_sale_;
+		}
+	
+};
+
+class Achats{
+	private:
+		const Article article;
+		const unsigned int quantity; 
+	public:
+		Achats(const Article& item,unsigned int quantity=1):
+				article(item),quantity(quantity){
+					}
+				
+		void afficher() const {
+			cout<<article.name()<<" :"<<article.price()<<" x "<<quantity<<" Frs";
+			
+			if(article.on_sale())
+			{
+				cout<<" = "<<article.price()*quantity*0.5<<"( en action)"<<endl;
+			}
+			else
+			{
+				cout<<" = "<<article.price()*quantity*0.5<<endl;
+			}
+		}
+		const Article& getArticle() const{
+			return article;
+		}
+		int Quantity() const{
+			return quantity;
+		}
+		double achat_price() const{
+			double prix(quantity*article.price());
+			if(article.on_sale())
+				prix*=0.5;
+			return prix;
+		}
+};
+class Caddie{
+	private:
+		vector<Achats> achats;
+	public:
+		void remplir(const Article& item,unsigned int quantity=1){
+				achats.push_back(Achats(item,quantity));
+		}
+		
+		double	total()	const	{
+			double	somme(0.0);
+			
+			for	(auto	const&	achat	:	achats)	{
+				achat.afficher();
+				somme	+=	achat.achat_price();
+			}
+		return	somme;
+		}
+		
+};	
+
+class Caisse{
+	private:
+		int till_no;
+		double total_amount;
+	public:
+		Caisse() : total_amount(0.0){
+			total_tills+=1;
+			till_no=total_tills;			
+		}
+		~Caisse(){
+			total_tills-=1;
+		}
+		
+		void afficher(){
+			cout<<"La caisse "<<till_no<<" a encaisse "<<total_amount<<" Frs audjourdhui"<<endl;
+		}
+		void scanner(const Caddie& caddie){
+			double local_total(caddie.total());			
+			total_amount+=local_total;
+			cout	<<	"-----"	<<	endl;
+			cout<<"Totale a payer : "<<local_total<<endl;			
+		}
+};
+
 
 // ======================================================================
 int main()
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+A coursera cours eon object oriented programming
